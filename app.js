181e92f5ede591de3542e03b30551ba5bc70f5d4
Belen Baez <bnoeliabelen@gmail.com>
2026-08-01 03:06:55 -0300

    agregar métodos para crear y eliminar turnos en la API

--- app.js
+++ app.js
@@ -13,9 +13,39 @@ let turnos = [
 ]
 
 app.get('/api/v1/turnos', (req, res) => {
-    res.json({ total: turnos.length,
+    res.status(200).json({ total: turnos.length,
     data: turnos 
    });
+});
+
+app.post('/api/v1/turnos', (req, res) => {
+    const { paciente, dni, especialidad } = req.body;
+
+      if (!paciente || !dni || !especialidad) {
+         return res.status(400).json({ error: 'Faltan datos obligatorios' });
+      }
+   const nuevoTurno = {
+      id: turnos.length + 1,
+      paciente,
+      dni,
+      especialidad
+   };
+
+   turnos.push(nuevoTurno);
+   res.status(201).json({ message: 'Turno creado correctamente', data: nuevoTurno });
+});
+
+app.delete('/api/v1/turnos/:id', (req, res) => {
+    const { id } = req.params;
+    const turnoExiste = turnos.some(t=> t.id === parseInt(id)); 
+    
+
+    if (turnoExiste === -1) {
+        return res.status(404).json({ error: 'Turno no encontrado' });
+    }
+
+    turnos.splice(turnoExiste, 1);
+    res.status(200).json({ message: 'Turno eliminado correctamente' });
 });
 
  app.listen(PORT, () => {
