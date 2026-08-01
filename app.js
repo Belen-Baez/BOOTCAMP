95fe0699e16c2113ca18299404de3b610a033d27
Belen Baez <bnoeliabelen@gmail.com>
2026-08-01 02:40:13 -0300

    eliminar rutas innecesarias y agregar endpoint para obtener lista de turnos

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-    res.send('  VENTANILLA DE SALA MUNICIPAL ABIERTA');
- });
- 
- app.get('/contacto', (req, res) => {
-    res.send(`
-        Datos de contacto:
-          Teléfono: 3777-234566
-          Email: contacto@empresa.com
-          Dirección: Calle San Martín 123
-          Web: www.empresa.com
 
-    `);
+app.use(express.json());
 
+let turnos = [
+    { id: 1, paciente: 'Fermin Perez', dni: '12345678', especialidad: 'Cardiología' },
+    { id: 2, paciente: 'Maria Lopez', dni: '87654321', especialidad: 'Dermatología' },
+    { id: 3, paciente: 'Juan Martinez', dni: '11223344', especialidad: 'Pediatría' },
+    { id: 4, paciente: 'Ana Torres', dni: '55667788', especialidad: 'Neurología' },
+    { id: 5, paciente: 'Carlos Ramirez', dni: '99887766', especialidad: 'Ginecología' }
+];
+
+app.get('/api/v1/turnos', (req, res) => {
+    res.json({ message: 'Lista de turnos' });
 });
+
  app.listen(PORT, () => {
     console.log(`servidor escuchando en http://localhost:${PORT}`);
  });
