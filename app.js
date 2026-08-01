83d40cfca1abdef0f6599fb7293ee2e6a493f018
Belen Baez <bnoeliabelen@gmail.com>
2026-08-01 02:45:55 -0300

    modificar endpoint para devolver lista de turnos con total y datos

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ let turnos = [
     { id: 3, paciente: 'Juan Martinez', dni: '11223344', especialidad: 'Pediatría' },
     { id: 4, paciente: 'Ana Torres', dni: '55667788', especialidad: 'Neurología' },
     { id: 5, paciente: 'Carlos Ramirez', dni: '99887766', especialidad: 'Ginecología' }
-];
+]
 
 app.get('/api/v1/turnos', (req, res) => {
-    res.json({ message: 'Lista de turnos' });
+    res.json({ total: turnos.length,
+    data: turnos 
+   });
 });
 
  app.listen(PORT, () => {
